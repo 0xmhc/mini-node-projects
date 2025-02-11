104562bc06d492995541ba9ae1cc472381046f5f
0xmhc <mohcinejawad783@gmail.com>
2025-02-11 16:59:37 +0100

    create script for asynchronous way

--- 1-Node-Farm/index.js
+++ 1-Node-Farm/index.js
@@ -1,11 +1,26 @@
 const fs = require("node:fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// blocking,synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `this is what we now about avocado. ${textIn}\n. Created on ${Date.now().toLocaleString()}`;
+// const textOut = `this is what we now about avocado. ${textIn}\n. Created on ${Date.now().toLocaleString()}`;
 
-fs.writeFileSync("./txt/output.txt", textOut);
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-console.log("file written!");
+// console.log("file written!");
+
+// blocking,asynchronous way
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Error");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      fs.writeFile("./txt/final.txt", `${data2} \n${data3}`, "utf-8", (err) => {
+        console.log("file has been written");
+      });
+    });
+  });
+});
+console.log("will read file");
